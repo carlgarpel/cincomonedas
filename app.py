73b1c22f951fa131b3e8f77c53070c1080e97312
Miguel Carlos García Pelaez <carlgarpel@gmail.com>
2023-07-04 12:06:42 +0200

    Index.html

--- app.py
+++ app.py
@@ -3,10 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    # a=""
-    # a.created.strftime('%d-%m-%Y')
-    return 'Aplicación para resolver un acertijo con 5 monedas. -En desarrollo.- '  
+def home():
+
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
