b3bbeda884d80dced4675aababac28479e81d46b
carlgarpel <carlgarpel@gmail.com>
2023-07-04 09:57:19 +0200

    Update app.py
    Corrijo un error inicial

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    a=""
-    a.created.strftime('%d-%m-%Y')
-    return 'Aplicación para resolver un acertijo con 5 monedas. -En desarrollo.- '  & a
+   
+   )
+    return 'Aplicación para resolver un acertijo con 5 monedas. -En desarrollo.- '
 
 if __name__ == '__main__':
     app.run()
